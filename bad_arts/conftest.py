1fd14e65191939211edccff166fc89cf3c872dfa
saoirse-defi <63586795+saoirse-defi@users.noreply.github.com>
2022-01-24 18:23:36 +0000

    inital commit

---
+++ bad_arts/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bad_arts.users.models import User
+from bad_arts.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
